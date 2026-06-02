de70d7671ae40ad8c219787741d3d66c10db1293
Jyothi Nandhan Repaka <nandhu6754@gmail.com>
2026-06-02 14:17:46 -0400

    Add Vercel web analytics

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
+
+inject();
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((error: unknown) => console.error(error));
-
